a789267676098858a1e40f2e268fe5fd74390525
Priyanka7685 <priyankathakur7685@gmail.com>
2025-07-13 16:45:10 +0530

    update

--- src/app/questions/[quesId]/[quesName]/edit/page.tsx
+++ src/app/questions/[quesId]/[quesName]/edit/page.tsx
@@ -3,7 +3,15 @@ import { databases } from "@/models/server/config";
 import React from "react";
 import EditQues from "./EditQues";
 
-const Page = async ({ params }: { params: { quesId: string; quesName: string } }) => {
+type PageProps = {
+  params: {
+    quesId: string;
+    quesName: string;
+  };
+};
+
+
+const Page = async ({ params }: PageProps) => {
     const question = await databases.getDocument(db, questionCollection, params.quesId);
 
     return <EditQues question={question} />;
